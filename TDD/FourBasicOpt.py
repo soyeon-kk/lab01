692759da7b4f2913823daab257809292d9aaaf0f
이소연 <84007823+soyeon-kk@users.noreply.github.com>
2025-10-21 06:58:03 +0900

    add FourBasicOpt.py

---
+++ TDD/FourBasicOpt.py
@@ -0,0 +1,18 @@
+# FourBasicOpt.py
+class FourBasicOpt:
+    """For the four basic arithmetic operations.
+    Class can be used to add(addition), subtract(subtraction),
+    divide(division) and multiply(multiplication).
+    """
+
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def divide(self, x, y):
+        return x / y
+
+    def multiply(self, x, y):
+        return x * y
